17aa46b789833d94d0c290211bacf749cedd13a7
ted <linyc1983@gmail>
2016-12-27 19:50:07 +0800

    add CRUD for t1 table

--- sqlite_1.sql
+++ sqlite_1.sql
@@ -1,1 +1,7 @@
 SELECT count(*) as cnt from sqlite_master where type ='table' and name='DEMO'
+
+CREATE table t1(t TEXT,i INTEGER)
+INSERT INTO t1 (t,i) VALUES ('abc',15)
+SELECT * from t1
+UPDATE t1 SET t = 'bbb' where i=15
+DROP table t1
