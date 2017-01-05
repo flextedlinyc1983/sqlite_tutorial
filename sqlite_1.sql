f76f8bbb0fc73e8ca9d468ef38fa99f330f69cd7
ted <linyc1983@gmail>
2017-01-05 17:19:32 +0800

    add some method

--- sqlite_1.sql
+++ sqlite_1.sql
@@ -5,3 +5,6 @@ INSERT INTO t1 (t,i) VALUES ('abc',15)
 SELECT * from t1
 UPDATE t1 SET t = 'bbb' where i=15
 DROP table t1
+
+var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+var db = window.openDatabase("PN_Database", "", "Backbone.WebSQL.Deferred", 1024 * 1024);
